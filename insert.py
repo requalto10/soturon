af616a7ad3019f1738e394e9c2aace943040fbad
requalto10 <fanjingtian1216@gmail.com>
2022-10-07 22:09:31 +0900

    軽微な修正

--- insert.py
+++ insert.py
@@ -2,17 +2,16 @@ import pandas as pd
 
 # データセットの読み込みと使う形への整形
 df =  pd.read_csv('./Mondrian/data/anonymized.csv', sep=",")
-dataset_size = 10 # エントリ数
-df_resized = df.loc[:(dataset_size-1)]
+dataset_size = 1000 # エントリ数
+df_resized = df.loc[:(dataset_size-1)] # 属性名以外の dataset_size 個のエントリ
 
 # 使う匿名化後データセット：指定したエントリ数だけリスト要素をもつリスト
 # [age, workclass, education-num, marital-status, occupation, race, sex, native-country, income]
 #  0    1          2              3               4           5     6    7               8
 list_dataset = df_resized.values.tolist()
 
-insert_entry = [18, 'Private', 6, 'Tech-support', 'Asian-Pac-Islander', 'Male', 'Japan', '<=50K'] # 追加エントリ
-
-# 数字の範囲 -> 数字のリスト　ex)['5~7'] -> [[5,6,7]]
+# 数値の範囲 -> 数値のリスト　ex)['5~7'] -> [[5,6,7]]
+# 今回は属性[0],[2]が数字
 for i in range(len(list_dataset)):
     age = list_dataset[i][0]
     e_num = list_dataset[i][2]
@@ -25,5 +24,13 @@ for i in range(len(list_dataset)):
         if c == '~':
             list_dataset[i][2] = list(range(int(e_num[:j]), int(e_num[j+1:]) + 1))
 
-print(list_dataset)
+
+# 追加エントリ
+insert_entry = [18, 'Private', 6, 'Tech-support', 'Asian-Pac-Islander', 'Male', 'Japan', '<=50K']
+
+
+
+
+
+print(len(list_dataset))
 
