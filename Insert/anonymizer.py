6e429054ad9ac16f9ce980c24080b25fafac30f4
requalto10 <fanjingtian1216@gmail.com>
2022-10-10 00:08:12 +0900

    single_dim_mondrian完成！

--- Insert/anonymizer.py
+++ Insert/anonymizer.py
@@ -10,32 +10,38 @@ from statistics import median
 # k : int
 # QI_index : 何番目のQiを partition に使用するか(0 ~ QIの数-1)
 def single_dim_mondrian(data, k, QI_index):
-    splitted_data = []
-    block_sizes = []
+    # multi_dim_mondrianは、引数でQI_indexを指定するのではなく、再帰ごとに指定する
 
-    # splitVal の決定
-    QI_values = []
-    for record in data:
-        values_index_count = 0
-        for value in record.values():
-            if values_index_count == QI_index:
-                QI_values.append(value[QI_index])
-            else:
-                values_index_count += 1
-    splitVal = median(QI_values) # partition に使用する閾値
-        
-    # data 内で lhs(>splitVal) rhs(<=splitVal) の順にレコードを並び替え
-    lhs = []
-    rhs = []
-    for record in data:
-        for value in record.values():
-            if value[QI_index] > splitVal:
-                lhs.append(record)
-            else:
-                rhs.append(record)
-    splitted_data.append(lhs)
-    splitted_data.append(rhs)
+    if len(data) < 2*k:
+        return data
+    else:
+        # splitVal（中央値）の決定
+        QI_values = []
+        for record in data:
+            values_index_count = 0
+            for value in record.values():
+                if values_index_count == QI_index:
+                    QI_values.append(value[QI_index])
+                else:
+                    values_index_count += 1
+        splitVal = median(QI_values) # partition に使用する閾値
 
-    return splitted_data
+        # split  
+        # data 内で lhs(>splitVal) rhs(<=splitVal) の順にレコードを並び替え
+        lhs = []
+        rhs = []
+        for record in data:
+            for value in record.values():
+                if value[QI_index] > splitVal:
+                    lhs.append(record)
+                else:
+                    rhs.append(record)
+
+        # 最後の1分割でkレコード未満になるブロックができてしまったら、その分割の前の結果を返す
+        if len(lhs) < k or len(rhs) < k:
+            return data 
+        else:
+            data = single_dim_mondrian(lhs, k, QI_index) + single_dim_mondrian(rhs, k, QI_index)
+            return data
    
     
--- Insert/test.py
+++ Insert/test.py
@@ -8,24 +8,4 @@ data = read_file.read_csv("Data/soturon_random_data.csv", True)
 result = anonymizer.single_dim_mondrian(data, 3, 0)
 print(result)
 
-""" print(type(data[0].values()))
-
-for value in data[0].values():
-    print(type(value[0])) """
-
-#print(type(data[0].values()))
-#print(data)
-
-book = {
-    'id': 123,
-    'authors': ['Author 1'],
-    'title': 'Title 1'
-}
-
-#book['id'] = book.pop('id')
-
-""" for i in book.values():
-    print(i, type(i))
- """
-
 
